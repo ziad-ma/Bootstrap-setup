d8e17325c39b3f865b9e833b3e5ad76c4a723b22
ziad mansour <ziadz_mansour@yahoo.com>
2020-11-08 19:37:35 +0200

    npm scripts ||

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('#mycarousel').carousel({ interval: 2000 });
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+    $('#ORT').click(function(){
+        $('#exampleModal').modal('show');
+    });
+
+    $('#CRT').click(function(){
+        $('#exampleModal').modal('hide');
+    });
+
+    $('#login').click(function(){
+        $('#loginModal').modal('show');
+    });
+
+    $('#C-login').click(function(){
+        $('#loginModal').modal('hide');
+    });
+});
